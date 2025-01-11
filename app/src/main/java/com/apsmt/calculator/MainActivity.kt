90e4604df3df80f9c64319b5f2d7cf5237fb478d
aungpyaekyaw <shwyzee109@gmail.com>
2025-01-11 12:55:11 +0630

    Decimal action and find percentage action

--- app/src/main/java/com/apsmt/calculator/MainActivity.kt
+++ app/src/main/java/com/apsmt/calculator/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.apsmt.calculator
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.apsmt.calculator.databinding.ActivityMainBinding
-
 
 
 class MainActivity : AppCompatActivity() {
@@ -18,11 +18,14 @@ class MainActivity : AppCompatActivity() {
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.btnClear.setOnClickListener { clearAllAction() }
         binding.btnBack.setOnClickListener { backSpaceAction() }
-        binding.btnPlus.setOnClickListener{addOperation(it)}
+        binding.btnPlus.setOnClickListener { addOperation(it) }
         binding.btnMinus.setOnClickListener { minusOperation(it) }
         binding.btnMultiply.setOnClickListener { multiplyOperation(it) }
         binding.btnDivide.setOnClickListener { dividedOperation(it) }
+        binding.btnPercentage.setOnClickListener { findPercentageOperation(it) }
+        binding.btnDecimal.setOnClickListener { addDecimalAction(it) }
 
         binding.btnOne.setOnClickListener { numberAction(it) }
         binding.btnTwo.setOnClickListener { numberAction(it) }
@@ -33,34 +36,42 @@ class MainActivity : AppCompatActivity() {
         binding.btnSeven.setOnClickListener { numberAction(it) }
         binding.btnEight.setOnClickListener { numberAction(it) }
         binding.btnNine.setOnClickListener { numberAction(it) }
-
+        binding.btnZero.setOnClickListener { numberAction(it) }
 
     }
 
     private fun numberAction(view: View) {
-        if(view is Button) {
+        if (view is Button) {
             binding.txvWorking.append(view.text)
         }
     }
 
-    private fun backSpaceAction(){
+    private fun backSpaceAction() {
         val length = binding.txvWorking.text.toString().length
-        if(length > 0) {
-            binding.txvWorking.text = binding.txvWorking.text.subSequence(0, length-1)
+        if (length > 0) {
+            binding.txvWorking.text = binding.txvWorking.text.subSequence(0, length - 1)
         }
+    }
+
+    private fun clearAllAction() {
+        binding.txvWorking.text = ""
+        binding.txvResult.text = ""
     }
 
     private fun operatorHandel(view: View) {
-        if(view is Button) {
-            binding.txvWorking.append(view.text)
+        if (view is Button) {
+            val workingText = binding.txvWorking.text.toString()
+            if(workingText.isNotEmpty() && workingText.last() !in "+-*/"){
+                binding.txvWorking.append(view.text)
+            }
         }
     }
 
-    private fun addOperation(view: View){
+    private fun addOperation(view: View) {
         operatorHandel(view)
     }
 
-    private fun minusOperation(view: View){
+    private fun minusOperation(view: View) {
         operatorHandel(view)
     }
 
@@ -70,6 +81,29 @@ class MainActivity : AppCompatActivity() {
 
     private fun dividedOperation(view: View) {
         operatorHandel(view)
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun findPercentageOperation(view: View) {
+        if (view is Button) {
+            try {
+                if (binding.txvWorking.text.isNotEmpty()) {
+                    binding.txvResult.text =
+                        (binding.txvWorking.text.toString().toDouble() / 100).toString()
+                }
+            } catch (e: Exception) {
+                binding.txvResult.text = "Error"
+            }
+        }
+    }
+
+    private fun addDecimalAction(view: View) {
+        if(view is Button) {
+            val workingText = binding.txvWorking.text.toString()
+            if(workingText.isNotEmpty() && workingText.last() !in "."){
+                binding.txvWorking.append(view.text)
+            }
+        }
     }
 
 
